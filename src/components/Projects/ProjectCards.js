17692de19c0c1ce04b51ed3a923f269e3ea585ed
Harshada <harshadajagtap199725@gmail.com>
2022-06-03 15:11:48 +0530

    updated image

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -6,12 +6,13 @@ import { BiLinkExternal } from "react-icons/bi";
 function ProjectCards(props) {
   return (
     <Card className="project-card-view">
-      <Card.Img variant="top" src={props.imgPath} alt="card-img" />
+      <Card.Img variant="top" src={props.imgPath} alt="card-img" height="220px" />
       <Card.Body>
         <Card.Title>{props.title}</Card.Title>
         <Card.Text style={{ textAlign: "justify" }}>
           {props.description}
         </Card.Text>
+        
         <Button variant="primary" href={props.gitlink} target="_blank">
           <BiLinkExternal /> &nbsp;
           {"View Project"}
