88ea9d77cf88f251aaf312cda1a97e03e0a8bfc9
Harshada <harshadajagtap199725@gmail.com>
2022-09-30 16:57:50 +0530

    contact me section

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,53 +7,49 @@ import { FaMedium } from "react-icons/fa";
 function Footer() {
   return (
     <Container fluid className="footer">
-      <Row>
-        <Col md="4" className="footer-body">
-          <ul className="footer-icons">
-            <li className="social-icons">
-              <a
-                href="https://github.com/harshadajagtap25"
-                style={{ color: "white" }}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <AiFillGithub />
-              </a>
-            </li>
+      <ul className="home-about-social-links">
+        <li className="social-icons">
+          <a
+            href="https://github.com/harshadajagtap25"
+            target="_blank"
+            rel="noreferrer"
+            className="icon-colour  home-social-icons"
+          >
+            <AiFillGithub />
+          </a>
+        </li>
 
-            <li className="social-icons">
-              <a
-                href="https://www.linkedin.com/in/harshada-jagtap-88334a192/"
-                style={{ color: "white" }}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <FaLinkedinIn />
-              </a>
-            </li>
-            <li className="social-icons">
-              <a
-                href="https://www.instagram.com/_harshu.jagtap/?hl=en"
-                style={{ color: "white" }}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <AiFillInstagram />
-              </a>
-            </li>
-            <li className="social-icons">
-              <a
-                href="https://medium.com/@harshadajagtap199725"
-                target="_blank"
-                rel="noreferrer"
-                className="icon-colour home-social-icons"
-              >
-                <FaMedium />
-              </a>
-            </li>
-          </ul>
-        </Col>
-      </Row>
+        <li className="social-icons">
+          <a
+            href="https://www.linkedin.com/in/harshada-jagtap-88334a192/"
+            target="_blank"
+            rel="noreferrer"
+            className="icon-colour  home-social-icons"
+          >
+            <FaLinkedinIn />
+          </a>
+        </li>
+        <li className="social-icons">
+          <a
+            href="https://www.instagram.com/_harshu.jagtap/?hl=en"
+            target="_blank"
+            rel="noreferrer"
+            className="icon-colour home-social-icons"
+          >
+            <AiFillInstagram />
+          </a>
+        </li>
+        <li className="social-icons">
+          <a
+            href="https://medium.com/@harshadajagtap199725"
+            target="_blank"
+            rel="noreferrer"
+            className="icon-colour home-social-icons"
+          >
+            <FaMedium />
+          </a>
+        </li>
+      </ul>
     </Container>
   );
 }
